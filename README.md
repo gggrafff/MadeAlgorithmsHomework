1e34007dd260b73294370a6bcfdc86a46f0b3a1b
gggrafff <tolik_azo@mail.ru>
2019-11-01 19:18:19 +0300

    Add Task6 Project

--- README.md
+++ README.md
@@ -6,9 +6,15 @@
 * Задача 2: https://contest.yandex.ru/contest/14656/problems/2_1/
 * Задача 3: https://contest.yandex.ru/contest/14768/problems/3_3/
 * Задача 4: https://contest.yandex.ru/contest/14768/problems/4/
+* Задача 5: 
+* Задача 6: https://contest.yandex.ru/contest/14885/problems/6_4/
+* Задача 7: 
 
 ## Посылки в контест
 * Задача 1: https://contest.yandex.ru/contest/14656/run-report/22581087/
 * Задача 2: https://contest.yandex.ru/contest/14656/run-report/22724909/
 * Задача 3: https://contest.yandex.ru/contest/14768/run-report/23215882/
 * Задача 4: https://contest.yandex.ru/contest/14768/run-report/23216436/
+* Задача 5: 
+* Задача 6: https://contest.yandex.ru/contest/14885/run-report/23545709/
+* Задача 7: 
---
+++ Task6/Task6.cpp
@@ -0,0 +1,176 @@
+﻿/*
+ * 6_4. Обход дерева в порядке level-order
+ * 
+ * Ограничение времени	0.4 секунды
+ * Ограничение памяти	64Mb
+ * 
+ * Дано число N < 106 и последовательность целых чисел из [-231..231] длиной N.
+ * Требуется построить бинарное дерево поиска, заданное наивным порядком вставки. 
+ * Т.е., при добавлении очередного числа K в дерево с корнем root, 
+ * если root→Key ≤ K, то узел K добавляется в правое поддерево root; иначе в левое поддерево root.
+ * Выведите элементы в порядке level-order (по слоям, “в ширину”).
+ *
+ * Формат ввода
+ * В первой строке записано число N.
+ * Во последующих N строках — целые числа исходной последовательности.
+ *
+ * Формат вывода
+ * N целых чисел, разделенных пробелом.
+ * 
+ * Контрольные примеры:
+ * In:
+ * 10
+ * 10
+ * 5
+ * 4
+ * 7
+ * 9
+ * 8
+ * 6
+ * 3
+ * 2
+ * 1
+ * Out:
+ * 10 5 4 7 3 6 9 2 8 1 
+ * 
+ * In:
+ * 10
+ * 9
+ * 8
+ * 6
+ * 10
+ * 4
+ * 3
+ * 2
+ * 5
+ * 1
+ * 7
+ * Out:
+ * 9 8 10 6 4 7 3 5 2 1 
+ * 
+ * In:
+ * 10
+ * 1
+ * 7
+ * 10
+ * 9
+ * 4
+ * 3
+ * 8
+ * 5
+ * 2
+ * 6
+ * Out:
+ * 1 7 4 10 3 5 9 2 6 8 
+ *
+ */
+
+/*
+ * Сложность:
+ * 					В среднем		В худшем случае (у дерева одна ветвь)
+ * 	Расход памяти	O(n)			O(n)
+ * 	Поиск			O(log n)		O(n)
+ * 	Вставка			O(log n)		O(n)
+ * 	Удаление		O(log n)		O(n)
+ */
+
+#include <iostream>
+#include <functional>
+#include <queue>
+
+namespace custom_containers
+{
+	template<typename T>
+	class BinarySearchTree
+	{
+	public:
+		explicit BinarySearchTree(T root)
+		{
+			key_ = root;
+		}
+		~BinarySearchTree()
+		{
+			delete left_;
+			delete right_;
+		}
+		BinarySearchTree(const BinarySearchTree& other) = delete;
+		BinarySearchTree(BinarySearchTree&& other) noexcept = delete;
+		BinarySearchTree& operator=(const BinarySearchTree& other) = delete;
+		BinarySearchTree& operator=(BinarySearchTree&& other) noexcept = delete;
+		void bfs(std::function<void(const T&)> action)
+		{
+			std::queue<BinarySearchTree*> elementsForProcessing;
+			elementsForProcessing.push(this);
+			while (!elementsForProcessing.empty())
+			{
+				auto currentElement = elementsForProcessing.front();
+				if (currentElement->left_ != nullptr)
+				{
+					elementsForProcessing.push(currentElement->left_);
+				}
+				if (currentElement->right_ != nullptr)
+				{
+					elementsForProcessing.push(currentElement->right_);
+				}
+				action(currentElement->key_);
+				elementsForProcessing.pop();
+			}
+		}
+		void insert(const T& value)
+		{
+			if (value < key_)
+			{
+				if (left_ == nullptr)
+				{
+					left_ = new BinarySearchTree(value);
+				}
+				else
+				{
+					left_->insert(value);
+				}
+			}
+			else
+			{
+				if (right_ == nullptr)
+				{
+					right_ = new BinarySearchTree(value);
+				}
+				else
+				{
+					right_->insert(value);
+				}
+			}
+		}
+
+	private:
+		T key_;
+		BinarySearchTree* left_{ nullptr };
+		BinarySearchTree* right_{ nullptr };
+	};
+}
+
+void processNumbers(const std::vector<int32_t>& numbers)
+{
+	custom_containers::BinarySearchTree<int32_t> tree(numbers.front());
+	for (size_t i = 1; i < numbers.size(); ++i)
+	{
+		tree.insert(numbers[i]);
+	}
+	tree.bfs([](const int32_t & number) {std::cout << number << " "; });
+}
+
+
+int main()
+{
+	size_t n{ 0 };
+	std::cin >> n;
+	std::vector<int32_t> numbers;
+	for (size_t i = 0; i < n; ++i)
+	{
+		auto number{ 0 };
+		std::cin >> number;
+		numbers.push_back(number);
+	}
+	processNumbers(numbers);
+}
+
