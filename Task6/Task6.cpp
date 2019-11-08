b5d7d9d79ef3c7a9be49efb47bb42d1b7b2fe0fc
gggrafff <tolik_azo@mail.ru>
2019-11-08 18:35:10 +0300

    убрал рекурсию из деструктора

--- Task6/Task6.cpp
+++ Task6/Task6.cpp
@@ -1,12 +1,12 @@
 ﻿/*
  * 6_4. Обход дерева в порядке level-order
- * 
+ *
  * Ограничение времени	0.4 секунды
  * Ограничение памяти	64Mb
- * 
+ *
  * Дано число N < 106 и последовательность целых чисел из [-231..231] длиной N.
- * Требуется построить бинарное дерево поиска, заданное наивным порядком вставки. 
- * Т.е., при добавлении очередного числа K в дерево с корнем root, 
+ * Требуется построить бинарное дерево поиска, заданное наивным порядком вставки.
+ * Т.е., при добавлении очередного числа K в дерево с корнем root,
  * если root→Key ≤ K, то узел K добавляется в правое поддерево root; иначе в левое поддерево root.
  * Выведите элементы в порядке level-order (по слоям, “в ширину”).
  *
@@ -16,7 +16,7 @@
  *
  * Формат вывода
  * N целых чисел, разделенных пробелом.
- * 
+ *
  * Контрольные примеры:
  * In:
  * 10
@@ -31,8 +31,8 @@
  * 2
  * 1
  * Out:
- * 10 5 4 7 3 6 9 2 8 1 
- * 
+ * 10 5 4 7 3 6 9 2 8 1
+ *
  * In:
  * 10
  * 9
@@ -46,8 +46,8 @@
  * 1
  * 7
  * Out:
- * 9 8 10 6 4 7 3 5 2 1 
- * 
+ * 9 8 10 6 4 7 3 5 2 1
+ *
  * In:
  * 10
  * 1
@@ -61,18 +61,18 @@
  * 2
  * 6
  * Out:
- * 1 7 4 10 3 5 9 2 6 8 
+ * 1 7 4 10 3 5 9 2 6 8
  *
  */
 
-/*
- * Сложность:
- * 					В среднем		В худшем случае (у дерева одна ветвь)
- * 	Расход памяти	O(n)			O(n)
- * 	Поиск			O(log n)		O(n)
- * 	Вставка			O(log n)		O(n)
- * 	Удаление		O(log n)		O(n)
- */
+ /*
+  * Сложность:
+  * 					В среднем		В худшем случае (у дерева одна ветвь)
+  * 	Расход памяти	O(n)			O(n)
+  * 	Поиск			O(log n)		O(n)
+  * 	Вставка			O(log n)		O(n)
+  * 	Удаление		O(log n)		O(n)
+  */
 
 #include <iostream>
 #include <functional>
@@ -83,7 +83,7 @@ namespace custom_containers
 
 	/**
 	 * \brief Класс, реализующий двоичное дерево поиска в целом и узел дерева в частности.
-	 * \tparam T Тип элементов, хранящихся в дереве. 
+	 * \tparam T Тип элементов, хранящихся в дереве.
 	 */
 	template<typename T>
 	class BinarySearchTree
@@ -101,7 +101,7 @@ namespace custom_containers
 		 * \brief Обход дерева в ширину.
 		 * \param action Функция, выполняемая над элементами.
 		 */
-		void bfs(std::function<void(const T&)> action);
+		void bfs(std::function<void(T&)> action);
 		/**
 		 * \brief Вставка элемента в дерево.
 		 * \param value Вставляемый элемент.
@@ -126,18 +126,21 @@ namespace custom_containers
 			 */
 			Node* right_{ nullptr };
 
-			explicit Node(const T& data = T()): key_(data), left_(nullptr), right_(nullptr) {}
-			~Node()
-			{
-				delete left_;
-				delete right_;
-			}
+			explicit Node(const T& data = T()) : key_(data), left_(nullptr), right_(nullptr) {}
+			~Node() = default;
 
 			Node(const Node& other) = delete;
 			Node(Node&& other) noexcept = delete;
 			Node& operator=(const Node& other) = delete;
 			Node& operator=(Node&& other) noexcept = delete;
 		};
+
+		/**
+		 * \brief Обход дерева в ширину.
+		 * \param action Функция, выполняемая над узлами.
+		 */
+		void bfsNodes(std::function<void(Node*)> action);
+
 		Node* root_{ nullptr };
 		std::function<bool(const T& lhs, const T& rhs)> less_{ std::less<T>() };
 	};
@@ -145,28 +148,16 @@ namespace custom_containers
 	template <typename T>
 	BinarySearchTree<T>::~BinarySearchTree()
 	{
-		delete root_;
+		bfsNodes(std::default_delete<Node>());
 	}
 
 	template <typename T>
-	void BinarySearchTree<T>::bfs(std::function<void(const T&)> action)
+	void BinarySearchTree<T>::bfs(std::function<void(T&)> action)
 	{
-		std::queue<Node*> elementsForProcessing;
-		elementsForProcessing.push(root_);
-		while (!elementsForProcessing.empty())
+		bfsNodes([&action](Node* node)
 		{
-			auto currentElement = elementsForProcessing.front();
-			if (currentElement->left_ != nullptr)
-			{
-				elementsForProcessing.push(currentElement->left_);
-			}
-			if (currentElement->right_ != nullptr)
-			{
-				elementsForProcessing.push(currentElement->right_);
-			}
-			action(currentElement->key_);
-			elementsForProcessing.pop();
-		}
+			action(node->key_);
+		});
 	}
 
 	template <typename T>
@@ -177,9 +168,9 @@ namespace custom_containers
 			root_ = new Node(value);
 			return;
 		}
-		Node* parent{nullptr};
+		Node* parent{ nullptr };
 		Node* insertPlace = root_;
-		while(insertPlace!=nullptr)
+		while (insertPlace != nullptr)
 		{
 			parent = insertPlace;
 			if (less_(value, insertPlace->key_))
@@ -201,6 +192,27 @@ namespace custom_containers
 			parent->right_ = insertPlace;
 		}
 	}
+
+	template <typename T>
+	void BinarySearchTree<T>::bfsNodes(std::function<void(Node*)> action)
+	{
+		std::queue<Node*> elementsForProcessing;
+		elementsForProcessing.push(root_);
+		while (!elementsForProcessing.empty())
+		{
+			auto currentElement = elementsForProcessing.front();
+			if (currentElement->left_ != nullptr)
+			{
+				elementsForProcessing.push(currentElement->left_);
+			}
+			if (currentElement->right_ != nullptr)
+			{
+				elementsForProcessing.push(currentElement->right_);
+			}
+			action(currentElement);
+			elementsForProcessing.pop();
+		}
+	}
 }
 
 
@@ -211,7 +223,7 @@ void processNumbers(const std::vector<int32_t>& numbers)
 	{
 		tree.insert(number);
 	}
-	tree.bfs([](const int32_t & number) {std::cout << number << " "; });
+	tree.bfs([](const int32_t& number) {std::cout << number << " "; });
 }
 
 
