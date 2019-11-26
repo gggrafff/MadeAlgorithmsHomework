6458174dfaae0791ebcd1b550b4951b3056c10f3
gggrafff <tolik_azo@mail.ru>
2019-11-26 20:15:31 +0300

    Задача 8 решена, работает. Написал условия остановки.

--- Task8/Task8.cpp
+++ Task8/Task8.cpp
@@ -8,24 +8,24 @@
 
 namespace custom_containers
 {
-    /**
+	/**
 	 * \brief Класс, реализующий хеш-таблицу открытой адресации с двойным хешированием.
-	 * \tparam T Тип элементов в хеш-таблице. Должен иметь метод data(), возвращающий указатель на данные, и метод size(), возвращающий количество элементов. 
+	 * \tparam T Тип элементов в хеш-таблице. Должен иметь метод data(), возвращающий указатель на данные, и метод size(), возвращающий количество элементов.
 	 */
 	template<typename T>
 	class HashMap
 	{
 	public:
-	/**
-         * \brief Конструктор по умолчанию.
-         */
+		/**
+			 * \brief Конструктор по умолчанию.
+			 */
 		HashMap() = default;
-        /**
-         * \brief Конструктор с возможностью задать пользовательские хеш-функции.
-         * \param hash1 первая основная хеш-функция
-         * \param hash2 вторая хеш-функция для разрешения коллизий
-         */
-        HashMap(const std::function<size_t(const T&)>& hash1, const std::function<size_t(const T&)>& hash2): hash1_(hash1), hash2_(hash2) {}
+		/**
+		 * \brief Конструктор с возможностью задать пользовательские хеш-функции.
+		 * \param hash1 первая основная хеш-функция
+		 * \param hash2 вторая хеш-функция для разрешения коллизий
+		 */
+		HashMap(const std::function<size_t(const T&)>& hash1, const std::function<size_t(const T&)>& hash2) : hash1_(hash1), hash2_(hash2) {}
 		~HashMap()
 		{
 			for (auto nodePtr : data_)
@@ -39,7 +39,7 @@ namespace custom_containers
 		HashMap& operator=(const HashMap& other) = delete;
 		HashMap& operator=(HashMap&& other) noexcept = delete;
 
-        /**
+		/**
 		 * \brief Вставка элемента в хеш-таблицу.
 		 * \param key Вставляемый элемент.
 		 * \return True, если вставка завершилась успешно, иначе - false.
@@ -79,6 +79,18 @@ namespace custom_containers
 			INVALID
 		};
 		/**
+		 * \brief Перечисление, содержащее возможные исходы, с которыми завершается пробирование.
+		 */
+		enum StoppingCondition
+		{
+			STOP_FOR_FREE = 1,
+			STOP_FOR_BUSY = 2,
+			STOP_FOR_DELETED = 4,
+			STOP_FOR_SEARCH = 3,
+			STOP_FOR_INSERT = 5,
+			STOP_FOR_ALL = 7,
+		};
+		/**
 		 * \brief Структура, реализующая ячейку, хранящуюся в таблице и содержащую данные.
 		 */
 		struct Node
@@ -90,7 +102,7 @@ namespace custom_containers
 		/**
 		 * \brief Тип, представляющий результат пробирования.
 		 */
-		using Position=std::pair<size_t, PositionType>;
+		using Position = std::pair<size_t, PositionType>;
 
 		//Объявления констант.
 		const size_t M = 4294967296ULL; // 2^32
@@ -102,9 +114,10 @@ namespace custom_containers
 		 * \brief Поиск позиции элемента в таблице.
 		 * \param key Элемент, для которого ищем позицию.
 		 * \param data Буфер хеш-таблицы, в котором подбираем позицию.
+		 * \param stoppingCondition Флаги, указывающие критерии остановки. Флаги выставляются с помощью enum StoppingCondition.
 		 * \return std::pair, в котором первый элемент - индекс в буфере, второй - исход пробирования типа PositionType.
 		 */
-		Position probePositionsInto(const T& key, std::vector<Node*>& data);
+		Position probePositionsInto(const T& key, std::vector<Node*>& data, uint8_t stoppingCondition = StoppingCondition::STOP_FOR_ALL);
 
 		/**
 		 * \brief Увеличивает размер хеш-таблицы вдвое.
@@ -124,7 +137,7 @@ namespace custom_containers
 		float calculateFullness() const;
 
 		/**
-		 * \brief 
+		 * \brief
 		 * \param key Хеш-функция по методу Горнера.
 		 * \param isMainHash Если true, то коэффициент a=5, иначе a=7.
 		 * \return Значение хеша.
@@ -132,7 +145,7 @@ namespace custom_containers
 		size_t hash(const T& key, bool isMainHash = true) const;
 
 		/**
-		 * \brief Один шаг пробирования с помощью двойного хеширования. 
+		 * \brief Один шаг пробирования с помощью двойного хеширования.
 		 * \param firstHash Значение первой хеш-функции.
 		 * \param secondHash Значение второй хеш-функции.
 		 * \param index Шаг пробирования.
@@ -145,8 +158,11 @@ namespace custom_containers
 		 * \brief Количество элементов в таблице.
 		 */
 		size_t size_{ 0 };
-
+		/**
+		 * \brief Массив, в котором хранятся элементы таблицы.
+		 */
 		std::vector<Node*> data_{ std::vector<Node*>(INITIAL_CAPACITY, nullptr) };
+
 		std::function<size_t(const T&)> hash1_{ std::bind(&HashMap::hash, this, std::placeholders::_1, true) };
 		std::function<size_t(const T&)> hash2_{ std::bind(&HashMap::hash, this, std::placeholders::_1, false) };
 	};
@@ -154,6 +170,10 @@ namespace custom_containers
 	template <typename T>
 	bool HashMap<T>::insert(const T& key)
 	{
+		if (contains(key))
+		{
+			return false;
+		}
 		if (isFull())
 		{
 			rehash();
@@ -179,8 +199,7 @@ namespace custom_containers
 	template <typename T>
 	bool HashMap<T>::remove(const T& key)
 	{
-		auto position = probePositionsInto(key, data_);
-		assert(position.second != PositionType::INVALID);
+		auto position = probePositionsInto(key, data_, StoppingCondition::STOP_FOR_SEARCH);
 		if (position.second == PositionType::BUSY)
 		{
 			data_[position.first]->deleted = true;
@@ -193,34 +212,33 @@ namespace custom_containers
 	template <typename T>
 	bool HashMap<T>::contains(const T& key)
 	{
-		auto position = probePositionsInto(key, data_);
-		assert(position.second != PositionType::INVALID);
+		auto position = probePositionsInto(key, data_, StoppingCondition::STOP_FOR_SEARCH);
 		return static_cast<bool>(position.second == PositionType::BUSY);
 	}
 
 	template <typename T>
-	typename HashMap<T>::Position HashMap<T>::probePositionsInto(const T& key, std::vector<Node*>& data)
+	typename HashMap<T>::Position HashMap<T>::probePositionsInto(const T& key, std::vector<Node*>& data, uint8_t stoppingCondition)
 	{
 		auto firstHash = hash1_(key);
 		auto secondHash = hash2_(key);
 		auto position = probe(firstHash, secondHash, 0) % data.size();
-		for (auto i = 0; i < data.size(); ++i)
+		for (size_t i = 0; i < data.size(); ++i)
 		{
-			if (data[position] == nullptr)
+			if (data[position] == nullptr && static_cast<bool>(stoppingCondition & StoppingCondition::STOP_FOR_FREE))
 			{
-				return {position, PositionType::FREE};
+				return { position, PositionType::FREE };
 			}
-			if (data[position]->key == key && !data[position]->deleted)
+			if (data[position]->key == key && !data[position]->deleted && static_cast<bool>(stoppingCondition & StoppingCondition::STOP_FOR_BUSY))
 			{
-				return {position, PositionType::BUSY};
+				return { position, PositionType::BUSY };
 			}
-			if (data[position]->deleted)
+			if (data[position]->deleted && static_cast<bool>(stoppingCondition & StoppingCondition::STOP_FOR_DELETED))
 			{
-				return {position, PositionType::DELETED};
+				return { position, PositionType::DELETED };
 			}
 			position = probe(firstHash, secondHash, i + 1) % data.size();
 		}
-		return {0, PositionType::INVALID};
+		return { 0, PositionType::INVALID };
 	}
 
 	template <typename T>
@@ -272,10 +290,10 @@ namespace custom_containers
 void processCommands(const std::vector<std::pair<std::string, std::string>>& commands)
 {
 	custom_containers::HashMap<std::string> hashMap;
-	for(auto& command: commands)
+	for (auto& command : commands)
 	{
 		auto result = false;
-		if(command.first == "+")
+		if (command.first == "+")
 		{
 			result = hashMap.insert(command.second);
 		}
@@ -302,19 +320,20 @@ std::vector<std::pair<std::string, std::string>> generateRandomCommands()
 {
 	srand(time(nullptr));
 	std::vector<std::pair<std::string, std::string>> commands;
-	for(auto i = 0; i<600;++i)
+	for (auto i = 0; i < 600; ++i)
 	{
 		const auto operation_number = rand() % 3;
 		std::string operation = "+";
-		if(operation_number == 1)
+		if (operation_number == 1)
 		{
 			operation = "-";
-		} else if (operation_number == 2)
+		}
+		else if (operation_number == 2)
 		{
 			operation = "?";
 		}
 		std::string str;
-		for(auto j=0;j<2;++j)
+		for (auto j = 0; j < 2; ++j)
 		{
 			str += 'a' + rand() % ('d' - 'a');
 		}
@@ -336,7 +355,7 @@ int main()
 		return 1;
 	}
 	std::vector<std::pair<std::string, std::string>> commands;
-	while(!in.eof())
+	while (!in.eof())
 	{
 		std::string operation;
 		std::string key;
